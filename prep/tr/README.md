09f3190c47d876f29dcbe9a7b8f9e74b0db965e0
Jamie <afflerbach@nceas.ucsb.edu>
2016-05-16 16:55:24 -0700

    init commit

---
+++ prep/tr/README.md
@@ -0,0 +1,12 @@
+## OHI+ US Northeast: Tourism & Recreation
+
+This folder describes the methods used to prepare data for Tourism & Recreation for the OHI+ US Northeast assessment.
+
+More information about this goal is available [here](http://ohi-science.org/goals/#tourism-and-recreation).
+                                                       
+The folders in this file include the metadata, R scripts, and data for the goal.
+                                                    
+                                                     
+Please see our [citation policy](http://ohi-science.org/citation-policy/) if you use OHI data or methods.
+                                                     
+Thank you!
---
+++ src/R/common.R
@@ -0,0 +1,42 @@
+#common.R
+
+#This script has common paths, libraries for the OHINE project
+
+
+# set the neptune data_edit share based on operating system
+dir_M             <- c('Windows' = '//mazu.nceas.ucsb.edu/ohi',
+                       'Darwin'  = '/Volumes/ohi',    ### connect (cmd-K) to smb://mazu/ohi
+                       'Linux'   = '/home/shares/ohi')[[ Sys.info()[['sysname']] ]]
+
+# WARN rather than stop if directory doesn't exist
+if (!file.exists(sprintf('%s/',dir_M))){
+  warning(sprintf("The directory for variable dir_M set in src/R/common.R does not exist. Do you need to mount %s?", dir_M))
+  
+}
+
+# install (if necessary) and load commonly used libraries
+packages <- c('dplyr', 'tidyr', 'stringr', 'readr', 'ggplot2')
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  cat(sprintf("Installing %s\n", setdiff(packages, rownames(installed.packages()))))
+  install.packages(setdiff(packages, rownames(installed.packages())))
+}
+
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(readr)
+library(ggplot2)
+rm(packages)
+
+### generic theme for all plots
+ggtheme_basic <- theme(axis.ticks = element_blank(),
+                       text = element_text(family = 'Helvetica', color = 'gray30', size = 8),
+                       plot.title = element_text(size = rel(1.25), hjust = 0, face = 'bold'),
+                       legend.position = 'right')
+
+ggtheme_plot <- ggtheme_basic +
+  theme(panel.border     = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_line(colour = 'grey90'),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "grey30"))
