138d34bc0b339c9701c51b54ed1b9271ebb4b670
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2016-10-10 10:05:30 -0700

    added libraries, projections, cols, tidyverse

--- src/R/common.R
+++ src/R/common.R
@@ -15,7 +15,7 @@ if (!file.exists(sprintf('%s/',dir_M))){
 }
 
 # install (if necessary) and load commonly used libraries
-packages <- c('dplyr', 'tidyr', 'stringr', 'readr', 'ggplot2')
+packages <- c('tidyverse','dplyr', 'tidyr', 'stringr', 'readr', 'ggplot2')
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   cat(sprintf("Installing %s\n", setdiff(packages, rownames(installed.packages()))))
   install.packages(setdiff(packages, rownames(installed.packages())))
@@ -24,18 +24,32 @@ if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
 #round everything to 2 decimal places
 options(digits=2)
 
-library(dplyr)
-library(tidyr)
+#libraries
+library(tidyverse)
 library(stringr)
-library(readr)
-library(ggplot2)
+library(RColorBrewer)
+
 rm(packages)
+
+#spatial information
+
+## extent for region of interest
+ne_ext <- raster::extent(-76, -62,37, 47)
+
+### set up proj4string options: NAD1983 and WGS84
+p4s_wgs84 <- '+proj = longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'
+p4s_nad83 <- '+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs +towgs84=0,0,0'
+
+
+### Define spectral color scheme for plotting maps
+cols      = rev(colorRampPalette(brewer.pal(9, 'Spectral'))(255)) # rainbow color scheme
 
 ### generic theme for all plots
 ggtheme_basic <- theme(axis.ticks = element_blank(),
                        text = element_text(family = 'Helvetica', color = 'gray30', size = 12),
                        plot.title = element_text(size = rel(1.25), hjust = 0, face = 'bold'),
                        legend.position = 'right')
+
 
 ggtheme_plot <- ggtheme_basic +
   theme(panel.border     = element_blank(),
