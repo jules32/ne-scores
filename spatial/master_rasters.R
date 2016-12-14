2bffed95a63574c37b1785ab4bc66752b8b50cfd
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2016-12-14 11:29:38 -0800

    added common.R source, removed redundant crs and ext, and recreated ocean_rgns.tif now that block island is back

--- spatial/master_rasters.R
+++ spatial/master_rasters.R
@@ -5,17 +5,12 @@
 
 #
 source('~/github/ohi-northeast/src/R/rast_tools.R')
+source('~/github/ohi-northeast/src/R/common.R')
 
 library(dplyr)
 library(raster)
 library(rgdal)
 
-## extent for region of interest
-wgs_ext <- raster::extent(-85, -55,30, 50) # this is larger than the actual NE extent. Only use this when cropping, then reprojecting to albers, and then crop again using the ne_ext
-ne_ext  <- extent(1750000, 2550000,300000,1200000) #this is for us_albers projection only
-
-### define US albers coordinate reference system (CRS)
-us_alb    <- crs("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") 
 #---------------------------------------------------------------------------------
 
 #create a base raster with 1000x1000, crs = us_alb and ne extent
@@ -49,7 +44,20 @@ writeRaster(ocean_ras,filename = 'ocean_rasters/ocean_ne.tif',overwrite=T)
 #the function gdal_rast2 comes from rast_tools.R
 rgn_ras <- gdal_rast2(src = '~/github/ohi-northeast/spatial/ne_ohi_rgns',
                       rast_base = ocean_ras,
-                      dst = 'ocean_rasters/ocean_rgns.tif',
+                      dst = '~/github/ohi-northeast/spatial/ocean_rasters/ocean_rgns.tif',
                       value = 'rgn_id',
                       override_p4s = TRUE)
+
+#-------------------------------------------------------------------------
+
+# create a raster at 1000 res for all cells within 3nm. We can use the state waters in our regions shapefile as the mask for this raster.
+
+three_nm <- rgns[rgns@data$rgn_id %in% c(1:7),]
+
+r_3nm_mask <- mask(ocean_ras,three_nm, progress='text',
+                   filename = '~/github/ohi-northeast/spatial/ocean_rasters/rast_3nm_mask.tif',overwrite=T)
+
+
+
+
 
