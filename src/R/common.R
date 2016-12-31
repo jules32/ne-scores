11a166d7f17151996ac16821c5776a49cb3639bf
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2016-12-30 16:07:39 -0800

    new states shapefile for land, added source for plot_scores function

--- src/R/common.R
+++ src/R/common.R
@@ -34,9 +34,9 @@ library(stringr)
 library(RColorBrewer)
 library(foreach)
 library(doParallel)
-library(raster)
 library(rgdal)
 library(parallel)
+library(raster)
 
 rm(packages)
 
@@ -44,32 +44,22 @@ rm(packages)
 
 ## extent for region of interest
 wgs_ext <- raster::extent(-85, -55,30, 50) # this is larger than the actual NE extent. Only use this when cropping, then reprojecting to albers, and then crop again using the ne_ext
-ne_ext  <- raster::extent(1719007,2564139,298989,1199438) #this is for us_albers projection only
-par(mar = c(2,2,1,1))
+ne_ext <- raster::extent(1750000, 2550000,300000,1200000) #this is for us_albers projection only
 
 ### set up proj4string options: NAD1983 and WGS84
 p4s_wgs84 <- '+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'
 p4s_nad83 <- '+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs +towgs84=0,0,0'
-us_alb    <- crs("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") 
+us_alb    <- raster::crs("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") 
 
 ### useful shapefiles
 #state land boundaries
-ne_states <- readOGR(dsn = file.path(dir_anx,'spatial'),layer = 'states', verbose = FALSE)%>%subset(STATE_NAME %in% c('Maine','New Hampshire','Massachusetts','Vermont','Connecticut','New York','Rhode Island', 'New Jersey','Delaware','Pennsylvania'))%>%
-              spTransform(us_alb)
-
-rgns <- readOGR(dsn = file.path(dir_git,'spatial'),layer = 'ne_ohi_rgns', verbose = FALSE)
-
-### create an ocean only raster for the region
-
-ocean_glob <- raster(file.path(dir_M,'model/GL-NCEAS-Halpern2008/tmp/ocean.tif'))
-# ocean_ne   <- ocean_glob%>%
-#               crop(extent(-7208377,-4464697,2652234,6643042))%>% #crop to smaller region to speed up process
-#               projectRaster(crs = us_alb,progress='text', res = 1000)%>%
-#                 crop(ne_ext, filename = '~/github/ohi-northeast/spatial/ocean_rasters/ocean_ne.tif')
-# ocean_rgns <- mask(ocean_ne,rgns,filename = '~/github/ohi-northeast/spatial/ocean_rasters/ocean_rgns.tif')
-
+ne_states  <- readOGR(dsn = paste0(path.expand(dir_git),'/spatial'),layer = 'states',verbose=F)
+rgns       <- readOGR(dsn = paste0(path.expand(dir_git),'/spatial'),layer = 'ne_ohi_rgns', verbose = FALSE) #need to use path.expand because readOGR does not read '~'
+rgns_simp  <- readOGR(dsn = paste0(path.expand(dir_git),'/spatial'),layer = 'ne_ohi_rgns_simp', verbose = FALSE) #need to use path.expand because readOGR does not read '~'
+rgn_data   <- rgns@data
 ocean_ne   <- raster('~/github/ohi-northeast/spatial/ocean_rasters/ocean_ne.tif')
 ocean_rgns <- raster('~/github/ohi-northeast/spatial/ocean_rasters/ocean_rgns.tif')
+zones      <- ocean_rgns #for zonal stats
 
 ### Define spectral color scheme for plotting maps
 cols      = rev(colorRampPalette(brewer.pal(9, 'Spectral'))(255)) # rainbow color scheme
@@ -87,3 +77,7 @@ ggtheme_plot <- ggtheme_basic +
         panel.grid.major = element_line(colour = 'grey90'),
         panel.background = element_blank(),
         axis.line = element_line(colour = "grey30"))
+
+### Source function that outputs a tmap score map
+
+source('~/github/ohi-northeast/src/R/plot_scores.R')
