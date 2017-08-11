695b8b13238c00f80b910d5efd5bc571defd5000
jules32 <jstewart@nceas.ucsb.edu>
2017-08-11 15:02:38 -0700

    updated about and faq webpages @jafflerbach @scarborough-courtney. goals page in progress...

--- _site.R
+++ _site.R
@@ -3,16 +3,24 @@ suppressPackageStartupMessages({
   library(readr)
   library(dplyr)
   library(tidyr)
+  library(stringr)
   library(knitr)
 })
 
 ## brewed vars
-study_area = "British Columbia"
-gh_repo    = "ohibc"
-
-## derived vars
-gh_url     = sprintf('https://github.com/OHI-Science/%s.git', gh_repo)
+study_area      <- "US Northeast"
+key             <- "ohi-northeast"
+dir_scenario_gh <- "https://raw.githubusercontent.com/OHI-Science/ohi-northeast/master/region2016"
 
 ## knitr options for all webpages
 knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
 
+## read in variables
+scores <- readr::read_csv(file.path(dir_scenario_gh, 'scores.csv'))
+layers <- readr::read_csv(file.path(dir_scenario_gh, 'layers.csv'))
+weight <- readr::read_csv(file.path(dir_scenario_gh, 'conf/goals.csv')) %>%
+  select(goal, weight)
+
+## save local copy of conf/goals.Rmd
+conf_goals <- readr::read_lines(file.path(dir_scenario_gh, 'conf/goals.Rmd'))
+readr::write_lines(conf_goals, path = 'conf_goals.Rmd', append = FALSE)
