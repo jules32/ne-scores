db1b0969afcf49f0c437ef3a4bcf0c13b998640a
jules32 <jstewart@nceas.ucsb.edu>
2017-11-13 11:02:43 -0800

    updated calculate_scores.r for multiple scenarios

--- region2016/calculate_scores.r
+++ region2016/calculate_scores.r
@@ -4,14 +4,38 @@
 ## - configure_toolbox.r ensures your files are properly configured for `ohicore`.
 ## - The `ohicore` function CalculateAll() calculates OHI scores.
 
-## set working directory for all OHI calculations
+## set working directory for all OHI calculations ----
 setwd("~/github/ohi-northeast/region2016")
 
-## run the configure_toolbox.r script to check configuration
-source("configure_toolbox.r")
+## load required packages. If not installed, install from the comment ----
+library(ohicore)   # source('~/github/ohi-northeast/install_ohicore.R')
+library(tidyverse) # install.packages('tidyverse')
+library(stringr)   # install.packages('stringr')
+library(zoo)       # install.packages('zoo')
 
-## calculate scenario scores
-scores <- ohicore::CalculateAll(conf, layers)
 
-## save scores as scores.csv
-readr::write_csv(scores, 'scores.csv', na='')
+## calculate scores for each year scenario and save to a single csv file ----
+
+## set scenario years, empty dataframe
+scenario_years <- c(2012:2017)
+scores_all_years <- data.frame()
+
+## loop through each scenario year
+for (s_year in scenario_years){  # s_year=2015
+  
+  ## configure checks
+  conf   <-  ohicore::Conf('conf')
+  layers <-  ohicore::Layers(layers.csv = 'layers.csv', layers.dir = 'layers')
+  layers$data$scenario_year <-  s_year 
+  
+  ## calculate scores
+  scores_sy <- ohicore::CalculateAll(conf, layers) %>%
+    mutate(year = s_year) 
+  
+  ## bind scores to dataframe
+  scores_all_years <- rbind(scores_all_years, scores_sy)
+  
+}
+
+## save .csv file
+readr::write_csv(scores_all_years, 'scores.csv', na='')
